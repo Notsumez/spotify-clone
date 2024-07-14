81fa4243c38de9c19dfa2736fbe9edadd07740ba
Andrey <adahorapirola@gmail.com>
2024-07-14 18:40:47 -0300

    Criação da Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import Sidebar from './components/Sidebar'
 
 const App = () => {
   return (
-    <div><h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1></div>
+    <div className='h-screen bg-black'>
+      <div className='h-[90%] flex'>
+        <Sidebar />
+      </div>
+    </div>
   )
 }
 
